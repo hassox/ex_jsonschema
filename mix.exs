4b560098e125dd0e2213319917fc99ced91f5048
hassox <dneighman@gmail.com>
2025-09-05 21:55:11 -0500

    Bump 0.1.7

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExJsonschema.MixProject do
   use Mix.Project
 
-  @version "0.1.6"
+  @version "0.1.7"
   @source_url "https://github.com/hassox/ex_jsonschema"
   @description "High-performance JSON Schema validation for Elixir using Rust"
 
